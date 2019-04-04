d21b5d9067a48da8d2e089fb5231ef19663d6622
Andrew <jaknowles1997@gmail.com>
2019-04-04 08:01:08 -0400

    initial front end file setup

---
+++ frontend/components/Nav.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+const Nav = () => (
+    <div>
+
+        <Link href="/sell">
+            <a>Selll!</a>
+        </Link>
+
+         <Link href="/">
+            <a>Home!</a>
+        </Link>
+
+    </div>
+)
+
+export default Nav
---
+++ frontend/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Home = props => (
+    <div>
+        <p>Home Page</p>
+
+    </div>
+)
+
+export default Home
